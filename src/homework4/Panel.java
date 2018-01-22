b6965ed3a3b4a362d3386fa1b96e55b0f701e873
chaimovy <chaimovy@gmail.com>
2018-01-22 20:24:52 +0200

    fufufufufufufu

--- src/homework4/Panel.java
+++ src/homework4/Panel.java
@@ -1,13 +1,21 @@
 package homework4;
 
+import java.awt.Color;
 import java.util.Observable;
 import java.util.Observer;
 
 public class Panel implements Observer {
-
+	private Color color;
+	private int id;
+	static int counter; 
+	public Panel(){
+		id=counter;
+		counter++;
+		
+	}
 	@Override
 	public void update(Observable o, Object arg) {
-		// TODO Auto-generated method stub
+		color=(Color)arg;
 		
 	}
 	//fhfgdfg f u 
